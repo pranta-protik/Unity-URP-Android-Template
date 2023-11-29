f7536dfbad705de80da6de642d2d3c7f62e26eae
Pranta Protik <pranta.cseku@gmail.com>
2023-11-29 10:52:48 +0600

    Menu item order fix

--- Assets/Toolbox/Scripts/Editor/ToolboxMenu.cs
+++ Assets/Toolbox/Scripts/Editor/ToolboxMenu.cs
@@ -4,13 +4,13 @@ namespace Toolbox.EditorScripts
 {
     public static class ToolboxMenu
     {
-        [MenuItem("Tools/Toolbox/Project Setup")]
+        [MenuItem("Tools/Toolbox/Project Setup", false, 1)]
         private static void ProjectSetup() => ProjectSetupWindow.InitWindow();
 
-        [MenuItem("Tools/Toolbox/Scene Setup")]
+        [MenuItem("Tools/Toolbox/Scene Setup", false, 2)]
         private static void SceneSetup() => SceneSetupWindow.InitWindow();
 
-        [MenuItem("Tools/Toolbox/Android Build Setup")]
+        [MenuItem("Tools/Toolbox/Android Build Setup", false, 3)]
         private static void AndroidBuildSetup() => AndroidBuildSetupWindow.InitWindow();
     }
 }
